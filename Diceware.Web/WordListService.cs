3d8578235e3bcce6e69b48242ceee75c433fec09
Sebastien ROBERT <sebastien.saigo@gmail.com>
2022-05-17 21:33:49 +0900

    Added super earlier version of the web application

---
+++ Diceware.Web/WordListService.cs
@@ -0,0 +1,19 @@
+﻿using Diceware.Library;
+
+namespace Diceware.Web;
+
+public class WordListService
+{
+    private readonly Task<Dictionary<int, string>> words;
+
+    public WordListService(HttpClient httpClient)
+    {
+        var wordList = new WordList(httpClient, "/diceware.wordlist.asc", new NullStorage());
+        words = wordList.GetWordList(false);
+    }
+
+    public Task<Dictionary<int, string>> GetWordList()
+    {
+        return words;
+    }
+}
